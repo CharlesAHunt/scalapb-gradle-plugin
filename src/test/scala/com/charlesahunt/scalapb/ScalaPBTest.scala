6cc42bc91f6932f0fe50c6e9408916f6ec12779b
Charles Hunt <charles.hunt@stackpath.com>
2017-10-03 16:58:37 -0500

    update build, small test tweak

--- src/test/scala/com/charlesahunt/scalapb/ScalaPBTest.scala
+++ src/test/scala/com/charlesahunt/scalapb/ScalaPBTest.scala
@@ -14,15 +14,11 @@ class ScalaPBTest extends WordSpec {
       def protocCommand(arg: Seq[String]) = com.github.os72.protocjar.Protoc.runProtoc(protocVersion +: arg.toArray)
 
       val targets = List(Target.apply(scalapb.gen(),
-        new File("src/test/scala/com/charlesahunt/scalapb")))
+        new File("src/test/compiled_protobuf")))
 
       val path = "src/test/scala/com/charlesahunt/scalapb/protos"
-      val incPath = List(s"-I/Users/charleshunt/projects/scalapb-gradle-plugin/src/test/scala/com/charlesahunt/scalapb/protos", s"-I$path/protobuf_external/google/protobuf/descriptor.proto",
-        s"-I$path/protobuf_external", s"-I$path/protobuf_external/google/protobuf/empty.proto", s"-I$path/protobuf_external/google/protobuf/type.proto", s"-I$path/protobuf_external/google/protobuf/timestamp.proto",
-        s"-I$path/protobuf_external/google/protobuf/source_context.proto", s"-I$path/protobuf_external/google/protobuf/wrappers.proto",
-        s"-I$path/protobuf_external/google/protobuf/field_mask.proto", s"-I$path/protobuf_external/google/protobuf/duration.proto",
-        s"-I$path/protobuf_external/google/protobuf/struct.proto", s"-I$path/protobuf_external/google/protobuf/plugin.proto",
-        s"-I$path/protobuf_external/google/protobuf/any.proto", s"-I$path/protobuf_external/google/protobuf/api.proto")
+      val incPath = List(s"-I/Users/charleshunt/projects/scalapb-gradle-plugin/src/test/scala/com/charlesahunt/scalapb/protos",
+        s"-I$path/protobuf_external/google/protobuf/descriptor.proto", s"-I$path/protobuf_external")
       val schemas = Set(new File(s"$path/Auth.proto"), new File(s"$path/Test.proto"))
 
       try {
