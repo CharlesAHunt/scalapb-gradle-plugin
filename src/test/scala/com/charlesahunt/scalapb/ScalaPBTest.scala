de3f87b36f305ece4b7a58852ba41006f553dca1
Charles <charlesalberthunt@gmail.com>
2018-07-15 03:32:23 -0500

    tweak tests

--- src/test/scala/com/charlesahunt/scalapb/ScalaPBTest.scala
+++ src/test/scala/com/charlesahunt/scalapb/ScalaPBTest.scala
@@ -13,24 +13,8 @@ class ScalaPBTest extends WordSpec {
   val testProjectDir = new TemporaryFolder
 
   "Protoc" should {
+
     "generate Scala classes from the given protos" in {
-
-      val currentPath = new java.io.File(".").getCanonicalPath + "/src/test/"
-      val files = ProtocPlugin.sourceGeneratorTask(
-        currentPath, //getProject.getProjectDir.getAbsolutePath,
-        "resources/protos", //projectProtoSourceDir,
-        List(),// internalProtoSources ++ externalProtoSources ++ extractedIncludeDirs,
-        "protosCompiled", //pluginExtensions.extractedIncludeDir,
-        "protosTarget", //targetDir,
-        true,//grpc
-        "-v360",
-        true
-      )
-
-      assert(files.nonEmpty)
-    }
-
-    "..." in {
       val currentPath = new java.io.File(".").getCanonicalPath + "/src/test/"
       val files = ProtocPlugin.sourceGeneratorTask(
         currentPath, //getProject.getProjectDir.getAbsolutePath,
@@ -41,6 +25,22 @@ class ScalaPBTest extends WordSpec {
         true,//grpc
         "-v360",
         false
+      )
+
+      assert(files.nonEmpty)
+    }
+
+    "generate scala classes from given protos when embedded is true" in {
+      val currentPath = new java.io.File(".").getCanonicalPath + "/src/test/"
+      val files = ProtocPlugin.sourceGeneratorTask(
+        currentPath, //getProject.getProjectDir.getAbsolutePath,
+        "resources/protos", //projectProtoSourceDir,
+        List(),// internalProtoSources ++ externalProtoSources ++ extractedIncludeDirs,
+        "protosCompiled", //pluginExtensions.extractedIncludeDir,
+        "protosTarget", //targetDir,
+        true,//grpc
+        "-v360",
+        true
       )
 
       assert(files.nonEmpty)
